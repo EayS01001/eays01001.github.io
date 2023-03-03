334f3c7115577bbd6e72b756e404efbe57eef52f
MS_school <983055238@qq.com>
2023-03-03 22:42:07 +0900

    自己紹介ページごとに要らないと判断

--- self-introduction.js
+++
@@ -1,9 +0,0 @@
-function view_self_introduction(){
-    $.ajax({
-        url: "self-introduction.html",
-        cache: false,
-        success: function(html){
-            document.write(html);
-        }
-    });
-}
